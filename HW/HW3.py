f0bfaadf93cfdd3ebd2bf0d21f98ef1544169e79
deathbuycactus <131403532+deathbuycactus@users.noreply.github.com>
2026-02-05 18:20:10 +0000

    HW 3; Q7

--- HW/HW3.py
+++ HW/HW3.py
@@ -6,10 +6,21 @@ from google import genai #import google.generativeai as genai
 
 # Show title and descr
 st.title("My HW 3 Question Answering Chatbot")
+st.write("This is a question answering chatbot. It takes up to two URLs and will attempt to answer questions you ask about them. " \
+"After your 4th prompt or after you respond that you do not want more information, it will summarize the interaction you had with it.")
 
 LLM = st.sidebar.selectbox("Which Model?",
                             ("ChatGPT", "Gemini"))
-
+def read_url_content(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status() # Raise an exception for HTTP errors
+        soup = BeautifulSoup(response.content, 'html.parser')
+        return soup.get_text()
+    except requests.RequestException as e:
+        print(f"Error reading {url}: {e}")
+        return None 
+    
 URL1 = URL1 = st.text_input(
     "Enter a URL",
     placeholder="https://example1.com"
@@ -42,9 +53,10 @@ if "messages" not in st.session_state:
             You are a question-answering assistant.
             You will answer questions that pertain to {URL1} and/or {URL2}. Do not forget the contents of these websites.
             End first response: 'Do you want more information?' 
-            If they want more information continue asking if they want more until they so no and return to default prompt. 
+            If they want more information continue asking if they want more until they so no, then summarize the conversation. 
             Keep your answers simple enough such that a ten year old can understand them.
-             """
+            If you reach 3 user-assistant exchanges, answer the user's question and provide a summary of the conversation as part of your response.
+            """
         },
         {
             "role": "assistant",
@@ -52,15 +64,7 @@ if "messages" not in st.session_state:
         }
     ]
 
-def read_url_content(url):
-    try:
-        response = requests.get(url)
-        response.raise_for_status() # Raise an exception for HTTP errors
-        soup = BeautifulSoup(response.content, 'html.parser')
-        return soup.get_text()
-    except requests.RequestException as e:
-        print(f"Error reading {url}: {e}")
-        return None 
+
     
 for msg in st.session_state.messages:
     if msg["role"] == "system":
